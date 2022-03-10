932aecf1b607356e2801ec69bbea88c00c83d5fc
Aian <aian.abdulaziz@feel-it-services.com>
2022-03-10 07:04:07 +0200

    ignore dist folder

--- dist/cjs/types/lib/components/CrontabInputGuru/valueHints/en.d.ts
+++
@@ -1,2 +0,0 @@
-declare let valueHints: string[][][];
-export default valueHints;
--- dist/cjs/types/lib/components/CrontabInputGuru/valueHints/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare const _default: {
-    en: string[][][];
-    zh_CN: string[][][];
-};
-export default _default;
